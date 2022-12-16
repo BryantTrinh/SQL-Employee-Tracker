17da7b89863caad0a241d852e773d806b42b4177
Bryant Trinh <trinhbryant@gmail.com>
2022-12-15 23:31:50 -0800

    revised connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,13 @@
-const mysql = require("mysql2");
+const mysql = require('mysql2');
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root123",
-  database: "employees"
+	host: 'localhost',
+	user: 'root',
+	password: '#',
+	database: 'employees',
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
+connection.connect(function (err) {
+	if (err) throw err;
 });
 
 module.exports = connection;
